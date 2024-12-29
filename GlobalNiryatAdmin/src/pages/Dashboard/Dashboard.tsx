c9e59b63d41e1881d7e395c613f6f356e2c974a7
AjinkyaBhagat <81403374+AjinkyaBhagat@users.noreply.github.com>
2024-12-29 17:32:42 +0530

    Added matrial UI,react router dom packages and pages folder.

---
+++ GlobalNiryatAdmin/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import "./Dashboard.module.css";
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="dashboard-container">
+      <h1>Dashboard</h1>
+      <p>Welcome to the Dashboard!</p>
+    </div>
+  );
+};
+
+export default Dashboard;
